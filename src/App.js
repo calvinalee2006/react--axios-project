a1bbc0649e7603bdb07cbe3b35b5d79092f7e390
Calvin Lee <Anthony2006Lee@hotmail.com>
2022-03-25 06:56:11 -0500

    tutorial

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imageURL: "",
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://dog.ceo/api/breeds/image/random")
+      .then((response) => {
+        this.setState({ imageURL: response.data.message });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    const { imageURL } = this.state;
+    return <img src={imageURL} />;
+  }
+}
+
+export default App;
